548cda404fbcff86794c2a6517858184cff8a004
unknown <rodrigo.daher@ufu.br>
2022-06-26 23:13:47 -0300

    Código final funcionando.

--- ProjetoDrive/ProjetoDrive.py
+++ ProjetoDrive/ProjetoDrive.py
@@ -1,3 +1,22 @@
-lojas = ['Rio de Janeiro', 'São Paulo', 'Belo Horizonte', 'Curitiba']
-for loja in lojas:
-    print(loja)
+import pyautogui
+import time
+
+# abrir o google drive no meu computador
+
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+time.sleep(0.25)
+pyautogui.write('https://drive.google.com/drive/my-drive')
+pyautogui.press('enter')
+
+# entrar na área de trabalho
+pyautogui.hotkey('win', 'd')
+
+# clicar no arquivo, arrastar e jogar no drive
+pyautogui.moveTo(1307, 54)
+pyautogui.mouseDown()
+pyautogui.moveTo(-641, 513)
+pyautogui.hotkey('alt', 'tab')
+time.sleep(1.5)
+pyautogui.mouseUp()
